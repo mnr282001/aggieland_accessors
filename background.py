a160eec1b7a87f9bcdf05cb8c1d7f7df6e6341f7
Nayab Rehmat <mnr282001@gmail.com>
2023-04-24 10:03:18 -0500

    gets color from click

--- background.py
+++ background.py
@@ -1,5 +1,23 @@
 from pynput import keyboard, mouse
 from PIL import ImageGrab
+from scipy.spatial import KDTree
+from webcolors import CSS3_HEX_TO_NAMES
+from webcolors import hex_to_rgb
+
+
+def convert_rgb_to_names(rgb_tuple):
+    
+    # a dictionary of all the hex and their respective names in css3
+    css3_db = CSS3_HEX_TO_NAMES
+    names = []
+    rgb_values = []
+    for color_hex, color_name in css3_db.items():
+        names.append(color_name)
+        rgb_values.append(hex_to_rgb(color_hex))
+    
+    kdt_db = KDTree(rgb_values)
+    distance, index = kdt_db.query(rgb_tuple)
+    return f'closest match: {names[index]}'
 
 
 def getHex(rgb):
@@ -10,7 +28,8 @@ def getColor(x,y):
     im = ImageGrab.grab(bbox=bbox)
     rgbim = im.convert('RGB')
     r,g,b = rgbim.getpixel((0,0))
-    print(f'COLOR: rgb{(r,g,b)} | HEX: #{getHex((r,g,b))}')
+    color_name = convert_rgb_to_names(tuple([r,g,b]))
+    print(color_name)
 
 
 def onClick(x,y,button,pressed):
