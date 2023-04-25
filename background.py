92cf316ef91273610e396046cdc68133a125d319
Nayab Rehmat <mnr282001@gmail.com>
2023-04-25 10:30:18 -0500

    added comments

--- background.py
+++ background.py
@@ -8,23 +8,22 @@ from playsound import playsound
 
 
 def getAudio(color_name):
-    # make request to google to get synthesis
+    # Make a request to Google Text-to-Speech (gTTS) API to get audio synthesis
     tts = gtts.gTTS(color_name)
 
-    # save the audio file
+    # Save the audio file
     tts.save("hello.mp3")
 
-    # play the audio file
+    # Play the audio file
     playsound("hello.mp3")
 
-    # remove the temp mp3 file
+    # Remove the temporary mp3 file
     if path.isfile("hello.mp3"):
         remove("hello.mp3")
 
 
 def convert_rgb_to_names(rgb_tuple):
-
-    # a dictionary of all the hex and their respective names in css3
+    # Create a dictionary of all the hexadecimal color codes and their respective names in CSS3
     css3_db = CSS3_HEX_TO_NAMES
     names = []
     rgb_values = []
@@ -32,16 +31,19 @@ def convert_rgb_to_names(rgb_tuple):
         names.append(color_name)
         rgb_values.append(hex_to_rgb(color_hex))
 
+    # Create a KDTree for fast nearest neighbor search on RGB values
     kdt_db = KDTree(rgb_values)
     distance, index = kdt_db.query(rgb_tuple)
     return f'closest match: {names[index]}'
 
 
 def getHex(rgb):
+    # Convert RGB tuple to hexadecimal color code
     return '%02X%02X%02X' % rgb
 
 
 def getColor(x, y):
+    # Get color of a pixel at (x, y) on the screen
     bbox = (int(x), int(y), int(x)+1, int(y)+1)
     im = ImageGrab.grab(bbox=bbox)
     rgbim = im.convert('RGB')
@@ -52,17 +54,20 @@ def getColor(x, y):
 
 
 def onClick(x, y, button, pressed):
+    # Handle left mouse button click event
     if pressed and button == mouse.Button.left:
         getColor(x, y)
 
 
 def onRel(key):
+    # Handle key release event, specifically Esc key to stop the listeners
     if key == keyboard.Key.esc:
         mlstnr.stop()
         return False
 
 
 if __name__ == '__main__':
+    # Start the keyboard and mouse listeners
     with keyboard.Listener(on_release=onRel) as klstnr:
         with mouse.Listener(on_click=onClick) as mlstnr:
             klstnr.join()
--- gui.py
+++ gui.py
@@ -1,23 +1,25 @@
 from tkinter import *
 from os import system
 
-
-root = Tk()
-root.title(" Aggieland Accessors ")
-
-root.geometry("600x350")
+root = Tk()  # Create a new Tkinter window
+root.title("Aggieland Accessors")  # Set the title of the window
+root.geometry("600x350")  # Set the initial dimensions of the window
 
 
 def run():
+    # Define a function to run the 'background.py' script when a button is clicked
     system('python3 background.py')
 
 
+# Create a button with a text label and a command to run the 'run' function when clicked
 btn_for_run = Button(
     root, text="Click Me to Start Detecting Color on Click! When you are done, click Escape!", command=run)
 
+# Create a button with a text label, a border width of 5, and a command to close the window when clicked
 btn_to_close = Button(root, text='Close me!', bd='5', command=root.destroy)
+# Add the 'btn_for_run' button to the window with vertical padding of 15 pixels
 btn_for_run.pack(pady=15)
+# Add the 'btn_to_close' button to the window with vertical padding of 15 pixels
 btn_to_close.pack(pady=15)
 
-
-root.mainloop()
+root.mainloop()  # Start the main event loop for the window, which handles user interactions and updates the window display
